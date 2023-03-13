eebebe6c8e3da0101b43ba474a903ab3ed5a3292
Aleksandr Konovalov <asrmod@mail.ru>
2023-03-13 20:25:36 +0400

    add new input

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,25 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text"; 
+      
+    formContainer.appendChild(input);
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) { 
+  document.querySelector('#form form').removeChild(otherInput); 
+  }
+});
+
 function increaseNumberAnimationStep (i, element, endNumber) {
   if (i <= endNumber) {
     if (i === endNumber) {
@@ -23,3 +43,4 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
